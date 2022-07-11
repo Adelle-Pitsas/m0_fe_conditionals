9c9b7206a1c25dcb7cf47072b1aa1b9b3d7b773b
Adelle Pitsas <apitsas4@gmail.com>
2022-07-11 17:59:49 -0400

    complete decisionMaking assignment

--- decisionMaking.js
+++ decisionMaking.js
@@ -3,7 +3,7 @@
 // Spend some time changing the variables and running the file to see how the story changes.
 
 
-var doorChoice = 1;
+var doorChoice = 3;
 var bearClothing = "";
 var bearChoice = 1;
 
@@ -35,9 +35,16 @@ if (bearChoice === 1) {
 
 // Questions
 // 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+// - Using an if statement, the code is defining the variable bearClothing based on whether the variable doorChoice is 1 or 2.
 // 2. What variable has a new value assigned to it after the first if statement executes?
+// - bearClothing
 // 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+// - "Scarf", because if it is anything other than 1, then it will be scarf.
 // 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+// - Using an if statement (and else/else if statements), depending on the variable "bearChoice", a series of conclusions will play out.
 // 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+// - If bearChoice was 3, then "You run as fast as you can into the next room. It's full of snakes!"
 // 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+// - If doorChoice was 1, then the bearClothing variable would be "hat". If the bearChoice was 2, then "You tell the bear the "hat" is too small and it starts to cry!
 // 7. What is your favorite ending?
+// - Best ending is definitely becoming friends with the bear.
